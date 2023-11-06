fd1b93e768f7bd1d54831a610ebe2e1d4f15e0c7
Samy Rahim <102865808+Samio735@users.noreply.github.com>
2023-11-06 12:15:53 +0100

    Update settings.py

--- ranking/settings.py
+++ ranking/settings.py
@@ -86,7 +86,15 @@ WSGI_APPLICATION = 'ranking.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bcranking',
