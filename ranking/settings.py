a927e0f811cbde23628ce731d9aea59ffa8d7c4d
Samy <s_rahim@estin.dz>
2023-11-18 23:27:39 +0100

    rm dotenv

--- ranking/settings.py
+++ ranking/settings.py
@@ -13,11 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # rest of your settings...
 
