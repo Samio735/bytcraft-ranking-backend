ca1fa083300bb2216d229cbf95425d0a45232db4
Samy <s_rahim@estin.dz>
2023-11-26 03:16:11 +0100

    dj admin

--- members/admin.py
+++ members/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Member 
+from .models import Member , Department , Activities 
 
 admin.site.register(Member)
+admin.site.register(Department)
+admin.site.register(Activities)
